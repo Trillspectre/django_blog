233cdb3e4cfa51fdc3f0ef88f8541440258730a4
Trillspectre <andrewbullimore@gmail.com>
2025-09-15 13:27:29 +0100

    added database and installed pakages for database dj-database-url psycopg2

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_UFTIlRJ52ejv@ep-polished-sky-agq1t82d.c-2.eu-central-1.aws.neon.tech/zesty_cycle_drive_347185"
+)
